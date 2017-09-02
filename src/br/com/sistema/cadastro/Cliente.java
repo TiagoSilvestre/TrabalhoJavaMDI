f74c1d71c63cbe97a54afef3e4f2bc1a967131b6
LAB\comp8 <alysonn0147@gamil.com>
2017-09-01 21:26:59 -0300

    Inicializando projeto

---
+++ src/br/com/sistema/cadastro/Cliente.java
@@ -0,0 +1,86 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.sistema.cadastro;
+
+/**
+ *
+ * @author comp8
+ */
+public class Cliente {
+    private String nome;
+    private String idade;
+    private String sexo;
+    private String cpf;
+    private String endereco;
+    private String telefone;
+
+    public Cliente(){
+        this.nome = "";
+        this.idade = "";
+        this.sexo = "";
+        this.cpf = "";
+        this.endereco = "";
+        this.telefone = "";
+    }
+    
+    public Cliente(String nome, String idade, String sexo, String cpf, String endereco, String telefone) {
+        this.nome = nome;
+        this.idade = idade;
+        this.sexo = sexo;
+        this.cpf = cpf;
+        this.endereco = endereco;
+        this.telefone = telefone;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+        
+    public String getIdade() {
+        return idade;
+    }
+
+    public void setIdade(String idade) {
+        this.idade = idade;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+}
